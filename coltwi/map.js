c5b4d35e4d3bc2b43a335e4c7ced469340585787
GrapeVigne <62226366+GrapeVigne@users.noreply.github.com>
2023-09-15 17:53:06 +1000

    get improved map html and get buttons to work correctly

--- coltwi/map.js
+++ coltwi/map.js
@@ -1,18 +1,20 @@
-var buttons = document.getElementsByTagName('button'),
-  questions = document.getElementsByTagName('div');
-for (var i = 0; i < buttons.length; i++) {
-  var button = buttons[i];
+let buttons = document.getElementsByTagName('button');
+let questions = document.getElementsByTagName('div');
+
+for (let i = 0; i < buttons.length; i++) {
+  let button = buttons[i];
   button.addEventListener('click', function() {
-    var target = this.getAttribute('data-next');
-    for (var j = 0; j < questions.length; j++) {
-      var question = questions[j];
+    let target = this.getAttribute('data-next');
+    this.parentElement.style.display = 'none';
+    for (let j = 0; j < questions.length; j++) {
+      let question = questions[j];
       if (question.id == target) {
         question.classList.add('show');
       }
     };
-    var height = document.body.scrollHeight;
+
+    let height = document.body.scrollHeight;
     window.scroll(0 , height);
-    //Get this to scroll to where you are working
   });
 };
 
@@ -29,6 +31,7 @@ document.querySelector('#pivotal-not-played').addEventListener('click', () => {
 function chooseRandom(spaces) {
   let len = spaces.length;
   if (len === 0) {
+    noMoreSpaces();
     spacesFinished();
   }
   else {
@@ -39,28 +42,35 @@ function chooseRandom(spaces) {
     }}
 };
 
-//Get this to hide the selection options when the array is empty
+function noMoreSpaces() {
+  const txt = document.getElementById("hide-text");
+  const but = document.getElementById("hide-button");
+  const pic = document.getElementById("hide-picture");
+  txt.classList.add('hide');
+  but.classList.add('hide');
+  pic.classList.add('hide');
+};
+
 function spacesFinished() {
-  let resultFinished = `<p>There are no spaces remaining to choose from.</p><p>Click the "Close Window" button below.</p>`;
+  let resultFinished = `<p>There are no spaces remaining to choose from.</p><p>You can close this window, or click "Begin Again" to run through all the spaces again.</p><br><button class="js-button" type="button" onclick="location.reload();">Begin Again</button>
+  `;
   document.querySelector('.result-finished').innerHTML = resultFinished;
-  let identifier = document.getElementsByTagName('.result')
-  identifier.classList.add('hide');
   return;
-}
+};
 
 function getRandomSpaceNotIndependent() {
   let resultText = '';
   let resultButton = '';
   let resultPicture = '';
   let chosenSpace = chooseRandom(spacesNotIndependent);
-  resultText = `<p>Does ${chosenSpace} qualify?</p>`
-  resultButton = `<br><button type='button' class='js-button'  onclick='javascript:window.close()'>Yes - Close Window</button><button type='button' class='js-button' onclick='getRandomSpaceNotIndependent();'>No - Select Another</button>`;
+  resultText = `<p>Does ${chosenSpace} qualify?</p><br>`
+  resultButton = `<button type='button' class='js-button' onclick='javascript:window.close()'>Yes - Close Window</button><button type='button' class='js-button' onclick='getRandomSpaceNotIndependent();'>No - Select Another</button>`;
   resultPicture = `<img src="images/${chosenSpace}.jpg">`
   document.querySelector('.result-text').innerHTML = resultText;
   document.querySelector('.result-button').innerHTML = resultButton;
   document.querySelector('.result-picture').innerHTML = resultPicture;
   return;
-}
+};
 
 document.querySelector('#pivotal-played').addEventListener('click', () => {
   getRandomSpaceIndependent();
@@ -79,3 +89,4 @@ function getRandomSpaceIndependent() {
   document.querySelector('.result-picture').innerHTML = resultPicture;
   return;
 }
+;
