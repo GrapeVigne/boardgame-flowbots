64e219adc397a9e8bdf2d9ec165e34145e14009e
GrapeVigne <62226366+GrapeVigne@users.noreply.github.com>
2023-09-26 18:38:38 +1000

    change index to hamburger menu

--- root/main.js
+++ root/main.js
@@ -1,6 +1,6 @@
+/*allows drop down menus in header nav*/
 const dropdownBtn = document.querySelectorAll(".dropdown-btn");
 const dropdown = document.querySelectorAll(".dropdown");
-const hamburgerBtn = document.getElementById("hamburger");
 const navMenu = document.querySelector(".menu");
 const links = document.querySelectorAll(".dropdown a");
 
